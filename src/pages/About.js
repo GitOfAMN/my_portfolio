379698a63ba757325e49d6cedc42b18f30df58f0
GitOfAMN <ashleynickens@gmail.com>
2023-04-04 05:01:13 -0400

    resume and about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,48 +1,58 @@
 // Index Page
+import React from 'react';
 
 export default function About(props) {
     return (
-        <div className='about'>
-            <p>For every complex problem, there is a creative solution; that's where I come in.<br></br><br></br>
+        <body>
+            <div class="container">
+            <div class="page">
+                    <div className='intro'>
+                        <h1>Hi, I'm Ashley.</h1><br></br>
+                        <h3>Full-Stack Software Engineer</h3>
+                    </div><br></br>
 
-                My inquisitive nature recently led me to delve into the world of software engineering. With this, I bring skills in building full-stack web applications, along with nearly 20 years combined experience and expertise in Program Management, customer service/sales and cross-functional team leadership, to the dynamic field of web development.<br></br><br></br>
+                    <div className='about'>
+                        <p> My inquisitive nature recently led me to delve into the world of software engineering. With this, I bring skills in building full-stack web applications, along with nearly 20 years combined experience and expertise in Client Relationship Management and most recently in Program Management, to the dynamic field of web development.<br></br><br></br>
 
-                My strategic mindset and attention to detail, along with my insatiable desire to learn and grow, enables me to design and develop innovative solutions for clients and businesses alike. I aim for optimization as I thrive on progress and continuous improvement, rather than perfectionism.<br></br><br></br>
+                            My strategic mindset and attention to detail, along with my insatiable desire to learn and grow, enables me to design and develop creative solutions for clients and businesses alike. I aim for optimization as I thrive on progress and continuous improvement, rather than perfectionism.<br></br><br></br>
 
-                I am a self-starter, highly motivated and driven individual with a growth mindset, who embraces the challenge of not having all the answers, and is not afraid to ask for help or use resources to find them. I excel in both team environments and independent settings, ultimately finding joy in presenting a finished product that I’ve helped create.<br></br><br></br>
+                            I am a self-starter, highly motivated and driven individual with a growth mindset, who embraces the challenge of not having all the answers, and is not afraid to ask for help or use resources to find them. I excel in both team environments and independent settings, ultimately finding joy in presenting a finished product that I’ve helped create.<br></br><br></br>
 
-                I build using:<br></br><br></br>
+                            I build using:<br></br><br></br>
 
-                <ul>
-                    <li>HTML</li>
-                    <li>JavaScript</li>
-                    <li>Python</li>
-                    <li>NodeJS</li>
-                    <li>ExpressJS</li>
-                    <li>React</li>
-                    <li>Bootstrap</li>
-                    <li>Flexbox</li>
-                    <li>CSS Grid</li>
-                    <li>MongoDB</li>
-                    <li>Mongoose</li>
-                    <li>PostgreSQL</li>
-                    <li>DOM Manipulation</li>
-                    <li>Event Handling</li>
-                    <li>REST</li>
-                    <li>MVC</li>
-                    <li>MERN</li>
-                    <li>Server Side Rendering</li>
-                    <li>Client Side Routing</li>
-                    <li>NoSQL Databases</li>
-                    <li>SQL</li>
-                    <li>Object Oriented Programming</li>
-                    <li>Functional Programming</li>
-                    <li>Async Programming</li>
-                    <li>Agile Scrum Methodologies</li>
-                    <li>Big O</li>
-                    <li>Algorithms and DataStructures in JS</li>
-                </ul>
-            </p>
-        </div>
+                            <ul>
+                                <li>HTML</li>
+                                <li>JavaScript</li>
+                                <li>Python</li>
+                                <li>NodeJS</li>
+                                <li>ExpressJS</li>
+                                <li>React</li>
+                                <li>Bootstrap</li>
+                                <li>Flexbox</li>
+                                <li>CSS Grid</li>
+                                <li>MongoDB</li>
+                                <li>Mongoose</li>
+                                <li>PostgreSQL</li>
+                                <li>DOM Manipulation</li>
+                                <li>Event Handling</li>
+                                <li>REST</li>
+                                <li>MVC</li>
+                                <li>MERN</li>
+                                <li>Server Side Rendering</li>
+                                <li>Client Side Routing</li>
+                                <li>NoSQL Databases</li>
+                                <li>SQL</li>
+                                <li>Object Oriented Programming</li>
+                                <li>Functional Programming</li>
+                                <li>Async Programming</li>
+                                <li>Agile Scrum Methodologies</li>
+                                <li>Big O</li>
+                                <li>Algorithms and DataStructures in JS</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </body>
     )
 }
